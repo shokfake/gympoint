7dc9e9c5a05280f4a36d14c5540d7d852a96be39
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-20 19:26:54 -0300

    Finalizando Desafio 02

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+	secret: 'e9092807c8c7c8cad0f68fcb0ff354e2',
+	expiresIn: '7d',
+};
---
+++ backend/src/database/index.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+
+// models
+import User from '../app/models/User';
+import Student from '../app/models/Student';
+
+import databaseConfig from '../config/database';
+
+const models = [User, Student];
+
+class Database {
+	constructor() {
+		this.init();
+	}
+
+	init() {
+		this.connection = new Sequelize(databaseConfig);
+
+		models.map(model => model.init(this.connection));
+	}
+}
+
+export default new Database();
