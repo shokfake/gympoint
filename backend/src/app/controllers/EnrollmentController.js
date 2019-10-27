d5852dbc176ea9d91099becdb630c0d752045367
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-10-27 16:50:55 -0300

    Validação dos dados

--- backend/src/app/controllers/EnrollmentController.js
+++ backend/src/app/controllers/EnrollmentController.js
@@ -130,6 +130,16 @@ class EnrollmentController {
 	}
 
 	async update(req, res) {
+		const schema = Yup.object().shape({
+			student_id: Yup.number(),
+			plan_id: Yup.number(),
+			start_date: Yup.date(),
+		});
+
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'Validation fails' });
+		}
+
 		const { enrollmentId } = req.params;
 
 		const enrollment = await Enrollment.findByPk(enrollmentId);
