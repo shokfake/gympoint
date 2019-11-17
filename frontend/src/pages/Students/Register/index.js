ed1f9052b0c31001eb4cde7e3f18ec03c60333f1
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-17 19:12:28 -0300

    Realizar cadastro

--- frontend/src/pages/Students/Register/index.js
+++ frontend/src/pages/Students/Register/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import * as Yup from 'yup';
 
 import { MdDone, MdChevronLeft } from 'react-icons/md';
+
+import api from '~/services/api';
+import history from '~/services/history';
 
 import Input from '~/components/Input';
 import Button from '~/components/Button';
@@ -8,7 +12,31 @@ import Form from '~/components/DefaultForm';
 
 import { Container } from './styles';
 
+const schema = Yup.object().shape({
+	name: Yup.string().required('O nome é obrigatório'),
+	email: Yup.string()
+		.email('Insira um e-mail válido')
+		.required('O e-email é obrigatório'),
+	idade: Yup.number()
+		.required('A idade é obrigatória')
+		.typeError('Insira uma idade válida'),
+	altura: Yup.number()
+		.required('A altura é obrigatória')
+		.typeError('Insira uma idade válida'),
+});
+
 export default function Register() {
+	async function hanldeSubmit({ name, email, idade, altura }) {
+		await api.post('students', {
+			name,
+			email,
+			idade,
+			altura,
+		});
+
+		history.push('/students');
+	}
+
 	return (
 		<Container>
 			<header>
@@ -19,14 +47,14 @@ export default function Register() {
 						<MdChevronLeft color="#fff" size={20} />
 						VOLTAR
 					</Button>
-					<Button type="button">
+					<Button type="submit" form="student-form">
 						<MdDone color="#fff" size={20} />
 						SALVAR
 					</Button>
 				</div>
 			</header>
 
-			<Form>
+			<Form schema={schema} onSubmit={hanldeSubmit} id="student-form">
 				<span>NOME COMPLETO</span>
 				<Input name="name" type="text" placeholder="Nome completo do aluno" />
 				<span>ENDEREÇO DE E-MAIL</span>
