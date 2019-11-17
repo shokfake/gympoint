78f94c66e3b0db27865d9a7ac5fe2261205f0867
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-17 18:28:27 -0300

    Adicionando novas rotas

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -8,14 +8,20 @@ import SignIn from '~/pages/SignIn';
 import Enrollments from '~/pages/Enrollments';
 import HelpOrders from '~/pages/HelpOrders';
 import Plans from '~/pages/Plans';
+
 import Students from '~/pages/Students';
+import StudentsRegister from '~/pages/Students/Register';
+import StudentsEdit from '~/pages/Students/Edit';
 
 export default function Routes() {
 	return (
 		<Switch>
 			<Route path="/" exact component={SignIn} />
 
-			<Route path="/students" component={Students} isPrivate />
+			<Route path="/students" exact component={Students} isPrivate />
+			<Route path="/students/register" component={StudentsRegister} isPrivate />
+			<Route path="/students/edit/:name" component={StudentsEdit} isPrivate />
+
 			<Route path="/plans" component={Plans} isPrivate />
 			<Route path="/enrollments" component={Enrollments} isPrivate />
 			<Route path="/helpOrders" component={HelpOrders} isPrivate />
