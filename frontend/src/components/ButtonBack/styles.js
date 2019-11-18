013fd169f18df76bd6a6c6b14a5a108a91a488dd
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-18 09:12:19 -0300

    Componentes de botões ja estilizados

---
+++ frontend/src/components/ButtonBack/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+	height: 36px;
+	font-size: 14px;
+	font-weight: bold;
+	color: #fff;
+	border: 0;
+	border-radius: 4px;
+	padding: 10px 16px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	background: #ccc;
+
+	svg {
+		margin-right: 8px;
+	}
+`;
---
+++ frontend/src/components/ButtonSave/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import colors from '~/styles/colors';
+
+export const Button = styled.button`
+	height: 36px;
+	font-size: 14px;
+	font-weight: bold;
+	color: #fff;
+	border: 0;
+	border-radius: 4px;
+	padding: 10px 16px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	margin-left: 16px;
+	background: ${colors.primary};
+
+	svg {
+		margin-right: 8px;
+	}
+`;
