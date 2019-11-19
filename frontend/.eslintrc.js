9526f8e69295afe2bf3c4e635869e6346ebd0069
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-19 14:04:16 -0300

    Adicionando regra no-alert

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -42,6 +42,7 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
 		"react-hooks/exhaustive-deps": "warn",
 		'react/jsx-props-no-spreading': 'off',
+		"no-alert": "off"
   },
   settings: {
     "import/resolver": {
