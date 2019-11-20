bb565ca971fd59dfd1ed14baf9f6acdac60d2d02
Elias Gabriel <eliasgabrielcf@gmail.com>
2019-11-20 12:30:07 -0300

    Imporatando componente Input estilizado

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Form, Input } from '@rocketseat/unform';
+import { Form } from '@rocketseat/unform';
 import * as Yup from 'yup';
+import Input from '~/components/Input';
 
 import { signInRequest } from '~/store/modules/auth/actions';
 
